cfe466d59795d3c1534f865e4e661f2acbdd25fc
Rob Cavanaugh <rob.cavanaugh@pitt.edu>
2021-09-19 13:45:03 -0400

    no slashes

--- docs/app.js
+++ docs/app.js
@@ -16,7 +16,7 @@ var randomimage = "Willa" + num + ".jpg";
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/serviceWorker.js")
+      .register("serviceWorker.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err))
   })
