c0c81eb257cb59920165ca2ccd9e523acc49ba95
Rob Cavanaugh <rob.cavanaugh@pitt.edu>
2021-09-18 21:53:09 -0400

    remainder

--- docs/app.js
+++ docs/app.js
@@ -1,7 +1,12 @@
-
-
-const date1 = dayjs()
+const date1 = dayjs();
 var weeks = date1.diff('2021-06-18', 'week');
-var text_out = 'Willa is now ' + weeks + ' weeks old.'
+var days = date1.diff('2021-06-18', 'days') % 7
+if (days>1){
+  var word_day = ' days'
+} else {
+  var word_day = ' day'
+}
+
+var text_out = 'Willa is now ' + weeks + ' weeks, ' + days + word_day + ' old.';
 
 
